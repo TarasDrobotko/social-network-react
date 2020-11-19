24857771d87929f7619ae558e367c7495b0d8ff1
tarasdrobotko <drobotkot@gmail.com>
2020-11-19 10:33:09 +0200

    add reducer

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   addMessageActionCreator,
   updateNewMessageActionCreator
-} from '../../redux/state';
+} from '../../redux/dialogs-reducer';
 import DialogItem from './DialogItem/DialogItem';
 
 import s from './Dialogs.module.css';
