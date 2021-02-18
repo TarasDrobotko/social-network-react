4c57b146976e0fe4bd19e548af891b9e1c9c67b4
tarasdrobotko <drobotkot@gmail.com>
2021-02-18 13:15:25 +0200

    login form: formik with react context

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,68 +1,56 @@
-import { Formik, Form, Field, ErrorMessage, useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import React from 'react';
 import s from './Login.module.css';
 
-const validate = (values) => {
-  const errors = {};
-  // Add the touched to avoid the validator validating all fields at once
-  if (!values.login) {
-    errors.login = 'Required';
-  }
-  if (!values.password) {
-    errors.password = 'Required';
-  } else if (values.password.length <= 8) {
-    errors.password = 'Must be more than 8 characters';
-  }
-  return errors;
-};
-
 const LoginForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      login: '',
-      password: '',
-      rememberMe: true
-    },
-    validate,
-    onSubmit: (values) => {
-      // Do stuff here...
-      alert(JSON.stringify(values, null, 2));
-    }
-  });
-
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div>
-        <input
-          name={'login'}
-          placeholder={'Login'}
-          onChange={formik.handleChange}
-          value={formik.values.login}
-        />
-        {formik.errors.login ? (
-          <div className={s.error}>{formik.errors.login}</div>
-        ) : null}
-      </div>
-      <div>
-        <input
-          name={'password'}
-          placeholder={'Password'}
-          onChange={formik.handleChange}
-          value={formik.values.password}
-        />
-        {formik.errors.password ? (
-          <div className={s.error}>{formik.errors.password}</div>
-        ) : null}
-      </div>
+    <Formik
+      initialValues={{
+        login: '',
+        password: '',
+        rememberMe: true
+      }}
+      validate={(values) => {
+        const errors = {};
+        // Add the touched to avoid the validator validating all fields at once
+        if (!values.login) {
+          errors.login = 'Required';
+        }
+        if (!values.password) {
+          errors.password = 'Required';
+        } else if (values.password.length <= 8) {
+          errors.password = 'Must be more than 8 characters';
+        }
+        return errors;
+      }}
+      onSubmit={(values) => {
+        // Do stuff here...
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <Field name="login" placeholder="Login" type="text" />
+            <ErrorMessage name="login" component="div" className={s.error} />
+          </div>
+          <div>
+            <Field name="password" placeholder="Password" type="text" />
+            <ErrorMessage name="password" component="div" className={s.error} />
+          </div>
 
-      <div>
-        <input name={'rememberMe'} type={'checkbox'} />
-        Remember me
-      </div>
-      <div>
-        <button>Login</button>
-      </div>
-    </form>
+          <div>
+            <Field name="rememberMe" type="checkbox" />
+            Remember me
+          </div>
+          <div>
+            <button type="submit" disabled={isSubmitting}>
+              Login
+            </button>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
